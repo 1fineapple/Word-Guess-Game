4fe955c941a731813499f13365e801cf2c4ae626
Jessa Molyneux <jessa@refinedrisk.com>
2018-10-09 23:08:01 -0400

    initializeD

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+var wins = 0;
+var words = ["trex", "allosaurus", "raptor"];
+var questions = [
+      { q: "This dinosaur has little arms", a: ["t,r,e,x"] },
+      { q: "definitely a carnivore", a: "a l l o s a u r u s" },
+      { q: "jurassic park fav", a: "r a p t o r" },
+      { q: "runnnnn", a: }
+]
+var questionIndex = 0;
+
+
+  //loop 2
+
+  // loop 3
+
+  renderQ
