1a8e6fadf504a1d0aeaae88506a1221c0b2f932f
Jessa Molyneux <jessa@refinedrisk.com>
2018-10-11 20:36:08 -0400

    new

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,67 +3,64 @@
 
 
 var wins = 0;
-var dinowords = ["trex", "allosaurus", "raptor, godzilla"];
-var image = ["Image1", "Image2", "Image3"];
-var word = dinowords[Math.floor(Math.random() * dinowords.length)];
-var countAttempts= 0
+
+var dinowords = ["trex", "allosaurus", "raptor", "godzilla"];
+var image = src= ["assets/images/t-rex.jpeg","assets/images/allosaurus.png","assets/images/raptor.jpeg","assets/images/godzilla.jpeg" ];
+
+
+// game begins
+
+var index = Math.floor(Math.random() * dinowords.length);
+var word = dinowords[index];
+var img = image[index];
+
+var gameDone = false;
+
+// var countAttempts= 0;
 
 var answerArray = [];
 var alreadyguessed=[];
 
-
- for (var i = 0; i < word.length; i++) {
- answerArray[i] = "_"; 
-
+for (var i = 0; i < word.length; i++) {
+  answerArray[i] = "_"; 
 }
 
+document.getElementById("answer").innerHTML = answerArray.join(" ");
 
-  // here you have push them into a string
+// while or else (!gameDone)?? goes where/ how to reference beginning of loop? {
 
-
-
-var word= "trex";
-var image = 0
+// }
 
 
-// while(dinowords[i]) {
-//   text += dinowords[i] + "<br>";
-//     i++;
-
-
-
-  // here you have print out (push) out- unless we are doing it down below?
-
-  // var remainingletters = word.length
-  // while(remainingLetters >= 0) {
-  
-
-  
-
- 
-
-  function checkLetter() {   
+function checkLetter() {   
       document.onkeyup = function(event) {    
        var guess = event.key.toLowerCase();
        
        var belongsTo = false 
        
+       var wordFinished = true;
+
         for (i = 0; i < word.length; i++) {
           if (guess === word[i]) {
             answerArray[i] = guess;
             document.getElementById("answer").innerHTML = answerArray.join(" ");
-          belongsTo = true
-      
+            belongsTo = true
           }
-          if (!belongsTo) {
-                alreadyguessed.push(guess);
-            document.getElementById("alreadyguessed").innerHTML = alreadyguessed.join(" ");
-             
 
-            }
-
-
+          if (word[i] === '_') {
+            wordFinished = false;
+          }
          }
+
+         if (!belongsTo) {
+          alreadyguessed.push(guess);
+          document.getElementById("alreadyguessed").innerHTML = alreadyguessed.join(" ");
+        }
+
+        if (wordFinished) {
+          gameDone = true;
+        }
+
        }
      }
     
