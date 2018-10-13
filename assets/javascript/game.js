7a85f3e4f8f2fa923e46b532e62af0f734f32d2d
Jessa Molyneux <jessa@refinedrisk.com>
2018-10-13 12:44:11 -0400

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,16 @@
 
 
-var wins = 0
+// these never change and are constant 
 var dinowords = ["trex", "allosaurus", "raptor", "godzilla"];
 var image = ["assets/images/t-rex.jpeg","assets/images/allosaurus.png","assets/images/raptor.jpeg","assets/images/godzilla.jpeg" ];
+var maxguess= 10;
 
-
-
-
-// game begins
-
+// these change as you play the game. 
+// click reset to change back to orginal values
 var index = Math.floor(Math.random() * dinowords.length);
 var word = dinowords[index];
 var imgSrc = image[index];
-var maxguess= 10
-
+var wins = 0
 var gameDone = false;
 var answerArray = [];
 var alreadyguessed=[];
@@ -29,14 +26,43 @@ document.getElementById("image").src = imgSrc;
 checkLetter();
 
 
-function checkLetter() {   
+function checkLetter() {
+  document.getElementById("resetbutton").addEventListener("click", function(event){
+  console.log("proof");
+
+  var index = Math.floor(Math.random() * dinowords.length);
+  var word = dinowords[index];
+  var imgSrc = image[index];
+  var wins = 0
+  var gameDone = false;
+  var answerArray = [];
+  var alreadyguessed=[];
+  
+  for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_"; 
+  }
+  
+  document.getElementById("answer").innerHTML = answerArray.join(" ");
+  document.getElementById("image").src = imgSrc;
+  document.getElementById("alreadyguessed").innerHTML = "Wrong Guesses";
+
+  }); 
+
       document.onkeyup = function(event) {    
        var guess = event.key.toLowerCase();
        var belongsTo = false 
        
        var wordFinished = true;
-       // if the word is finished count as win++, if alreadyguessed<maxguess; lose ++ 
-       // if condition 1 if condition 2 elses   alreadyguessed.length maxguess 
+       
+      //  if wordfinished {
+      //    wins++; newgame()
+        
+       
+       for (var i= 0;i <maxguess.length; i++){
+        alreadyguessed=[i]
+       }
+       
+      //  document.getElementById("guesses").innerHTML = alreadyguessed.join(" ");
 
 
         for (i = 0; i < word.length; i++) {
@@ -56,11 +82,9 @@ function checkLetter() {
           document.getElementById("alreadyguessed").innerHTML = alreadyguessed.join(" ");
         }
         
-        if(wordFinished) {
-          //newGame();
-        }
+      }
     } 
-}
+
     
      
          
